f0a62232553e9821b4cf21fc992d5fe34516532a
Lastercho <55585714+Lastercho@users.noreply.github.com>
2022-10-24 16:58:07 +0300

    Update omada-install.sh

--- setup/omada-install.sh
+++ setup/omada-install.sh
@@ -70,8 +70,8 @@ sudo dpkg -i mongodb-org-server_3.6.23_amd64.deb &>/dev/null
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Omada Controller"
-wget -qL https://download.tplinkcloud.com/firmware/omada_v5.5.6_linux_x64_20220729090108_1661156231493.deb
-sudo dpkg -i omada_v5.5.6_linux_x64_20220729090108_1661156231493.deb &>/dev/null
+wget -qL https://download.tplinkcloud.com/firmware/omada_v5.6.3_linux_x64_20220921083148_1666595513596.deb
+sudo dpkg -i omada_v5.6.3_linux_x64_20220921083148_1666595513596.deb &>/dev/null
 msg_ok "Installed Omada Controller"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
